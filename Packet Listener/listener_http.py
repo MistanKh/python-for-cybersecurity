76969374361315fc45cd24ebb4528e90d3213fbd
MistanKh <mistankhomdram@gmail.com>
2026-03-31 17:19:46 +0530

    Document packet listener flow

--- Packet Listener/listener_http.py
+++ Packet Listener/listener_http.py
@@ -4,6 +4,7 @@ import optparse
 
 
 def user_input():
+    # Read the interface name from the command line so the script can be reused across systems.
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Specify the interface to scan")
     (options, args) = parser.parse_args()
@@ -15,6 +16,7 @@ def packet_listener(interface):
 
 
 def analyze_packet(packet):
+    # Only print application data for packets that contain an HTTP request and raw payload bytes.
     if packet.haslayer(http.HTTPRequest):
         if packet.haslayer(scapy.Raw):
             print(packet[scapy.Raw].load())
